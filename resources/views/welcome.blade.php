08941b548e6227181b4f8a53a615b683d15c5302
Helton Denis <heltondeniss@gmail.com>
2019-12-01 00:52:59 -0300

    Blade initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,122 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Laravel</title>
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+      <!-- Styles -->
+      <style>
+         .bd-placeholder-img {
+         font-size: 1.125rem;
+         text-anchor: middle;
+         -webkit-user-select: none;
+         -moz-user-select: none;
+         -ms-user-select: none;
+         user-select: none;
+         }
 
-        <title>Laravel</title>
+         .btn-action-voto {
+            text-align: right;
+            font-size: 25px;
+            margin: 5px;
+         
+        }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        .card.mb-3 {
+            -webkit-box-shadow: 7px 10px 24px -16px rgba(0,0,0,0.75);
+            -moz-box-shadow: 7px 10px 24px -16px rgba(0,0,0,0.75);
+            box-shadow: 7px 10px 24px -16px rgba(0,0,0,0.45);
+        }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+            font-size: 3.5rem;
             }
+         }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      </style>
+   </head>
+   <body>
+      <div class="flex-center position-ref full-height">
+         @if (Route::has('login'))
+         <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a> @else
+            <a href="{{ route('login') }}">Login</a> @if (Route::has('register'))
+            <a href="{{ route('register') }}">Registrar</a> @endif @endauth
+         </div>
+         @endif
+         <body>
+            <header>
+               <div class="collapse bg-dark" id="navbarHeader">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-sm-8 col-md-7 py-4">
+                           <h4 class="text-white">Teste</h4>
+                           <p class="text-muted"></p>
+                        </div>
+                        <div class="col-sm-4 offset-md-1 py-4">
+                           <ul class="list-unstyled">
+                              <li><a href="#" class="text-white">Teste</a></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="navbar navbar-dark bg-dark shadow-sm">
+                  <div class="container d-flex justify-content-between">
+                     <a href="#" class="navbar-brand d-flex align-items-center">
+                     <strong>Aplicacao</strong>
+                     </a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                     </button>
+                  </div>
+               </div>
+            </header>
+            <main role="main">
+               <section class="jumbotron text-center">
+                  <div class="container">
+                     <h1></h1>
+                     <p class="lead text-muted"></p>
+                  </div>
+               </section>
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                     <div class="card mb-3">
+                        <div class="card-body">
+                        <h5 class="card-title">João Augusto</h5>
+                        <p class="card-text">Quantos anos preciso para me aposentar?</p>
+                        <hr>
+                        <p class="card-text"><small class="text-muted">
+                            <div class="btn-action-voto" >
+                                <a href="">
+                                <i class="fas fa-thumbs-down" style="color:black"></i>
+                                </a>
+                          
+                                <a href="">
+                                    <i class="fas fa-thumbs-up" style="color:#00995D"></i>
+                                </a>
+                            </div>
+                            </small>
+                         </p>
+                       
+                        </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </main>
+            <footer class="text-muted">
+               <div class="container">
+               </div>
+            </footer>
         </div>
-    </body>
+      </div>
+   </body>
 </html>
